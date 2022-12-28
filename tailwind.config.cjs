faa781c218078bbea100dafee34cb3d6002bd4f2
JoseArjona <j.angel.matheis@gmail.com>
2022-12-28 17:53:14 -0600

    Habilitar Modo Oscuro, Landing Page, Otros

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {},
+		fontFamily: {
+			sans: ['Inter','system-ui' ,'sans-serif'],
+			mono: ['Red Hat Mono','Consolas', 'SFMono-Regular'],
+		},
 	},
 	plugins: [],
 }
