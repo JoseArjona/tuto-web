1c50af6c713ab3a4320884ffb65c4f25742e9618
JoseArjona <j.angel.matheis@gmail.com>
2023-01-13 21:20:57 -0600

    nombre

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'JCEKM_TUTO';
-export const SITE_DESCRIPTION = 'Bienvenidos a JCEKM_TUTO, una web de JCEKM! Tutoriales y guias sobre el mundo de la tecnologia.';
+export const SITE_TITLE = 'MEXIC0DE-TUTO';
+export const SITE_DESCRIPTION = 'Bienvenidos a MEXIC0DE_TUTO, una web de MEXIC0DE! Tutoriales y guias sobre el mundo de la tecnologia.';
